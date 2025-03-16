45d8c513bf60bd526aabf7559fb692dca3d127ed
Janetta Doncheva <janettadoncheva@gmail.com>
2025-03-16 15:28:01 +0000

    adding a new ticket

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -8,14 +8,14 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  @Output() add = new EventEmitter<{ title: string; text: string }>();
 
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
+    this.add.emit({ title: title, text: ticketText });
 
-    onSubmit(title: string, taxt: string, form: HTMLFormElement){
-    
-        form.reset();
-    }
-
+    form.reset();
+  }
 }
